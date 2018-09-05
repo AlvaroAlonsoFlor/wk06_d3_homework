b41c4e8d7cf1bcc378d4bb00d6e343628b5a2728
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-05 20:35:59 +0100

    Add basic structure to toUpperCaser

--- hw_advanced_enumeration/upper_caser/upper_caser.js
+++ hw_advanced_enumeration/upper_caser/upper_caser.js
@@ -1,7 +1,12 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
+  const result = []
+  return this.words[0].toUpperCase()
+
+  return result.toUpperCase()
 
 }
 
