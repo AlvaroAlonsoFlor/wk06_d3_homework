cbeca31d040a00468af977f776e9f59b7d20ec24
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-05 23:10:06 +0100

    back to isogram start

--- hw_advanced_enumeration/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration/isogram_finder/isogram_finder.js
@@ -6,22 +6,9 @@ const IsogramFinder = function (word) {
 
 IsogramFinder.prototype.isIsogram = function () {
   //should use every
-  const word = this.word;
-
-  return letters.every( (letter) => word.includes(letter) );
 
 }
 
-IsogramFinder.prototype.createLetters = function () {
-  const letters = []
 
-  this.word.forEach((item) => {
-    if (!letters.includes(item)) {
-      letters.push(item)
-    }
-  });
-
-  return letters
-};
 
 module.exports = IsogramFinder;
--- hw_advanced_enumeration/isogram_finder/isogram_finder_spec.js
+++ hw_advanced_enumeration/isogram_finder/isogram_finder_spec.js
@@ -9,12 +9,12 @@ describe('IsogramFinder', function () {
 
   it('should be able to detect a non-isogram', function () {
     const isogramFinder = new IsogramFinder('repeated');
-    console.log('testing:', isogramFinder.createLetters());
     assert.strictEqual(isogramFinder.isIsogram(), false);
   });
 
   it('should be able to detect an isogram case insensitively', function () {
     const isogramFinder = new IsogramFinder('Uncopyrightable');
+    console.log('testing:', isogramFinder.createLetters());
     assert.strictEqual(isogramFinder.isIsogram(), true);
   });
 
