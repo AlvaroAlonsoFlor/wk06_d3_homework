79ea8a587f68ff633915380d7d79bd13ece05e63
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-05 23:37:06 +0100

    Check length

--- hw_advanced_enumeration/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration/anagram_finder/anagram_finder.js
@@ -1,9 +1,17 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('')
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
+
+  let result = otherWords.filter ( (otherWord) => {
+    return otherWord.length === this.word.length;
+  });
+
+  return result
 }
+
+
 
 module.exports = AnagramFinder;
