bff2b8fc1fb205f7c64e72c4a5c54b7fcd001cb0
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-06 00:28:07 +0100

    Finish anagram

--- hw_advanced_enumeration/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration/anagram_finder/anagram_finder.js
@@ -1,12 +1,12 @@
 const AnagramFinder = function (word) {
-  this.word = word.toLowerCase().split('')
+  this.word = word.toLowerCase().split('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
 
   let result = otherWords.filter ( (otherWord) => {
-    return otherWord.length === this.word.length;
+    return otherWord.length === this.word.length && otherWord.toLowerCase() !== this.word.join('') ;
   });
 
   result = result.filter( (otherWord) => {
