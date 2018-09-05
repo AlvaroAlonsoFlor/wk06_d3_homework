611fe2ac79c3de226d6152af348557ed2f3d20b4
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-05 22:28:27 +0100

    Add basic structure to isIsogram

--- hw_advanced_enumeration/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration/isogram_finder/isogram_finder.js
@@ -1,10 +1,14 @@
 const IsogramFinder = function (word) {
   this.word = word.toLowerCase()
+  // letters = letters without repetition
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
   //should use every
+  const word = this.word.split('')
+
+  letters.every( (letter) => word.includes(letter) )
 
 }
 
