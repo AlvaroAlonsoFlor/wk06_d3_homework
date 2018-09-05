081489563a67d1966baf50830faf8cd8f5df6ed1
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-06 00:06:12 +0100

    Filter letters

--- hw_advanced_enumeration/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration/anagram_finder/anagram_finder.js
@@ -9,7 +9,14 @@ AnagramFinder.prototype.findAnagrams = function (otherWords) {
     return otherWord.length === this.word.length;
   });
 
-  return result
+  result = result.filter( (otherWord) => {
+    return otherWord.toLowerCase().split('').every( (letter) => {
+      return this.word.includes(letter);
+      //if it's true it will filter it, otherwise not
+    });
+  });
+
+  return result;
 }
 
 
